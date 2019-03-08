88603ec0902164ef705b9302d0b925bd0baedf5c
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-03-08 09:07:08 +0100

    Create base project

---
+++ Pense-Bet/src/main/java/ch/arc/pensebet/PenseBetApplication.java
@@ -0,0 +1,13 @@
+package ch.arc.pensebet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenseBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenseBetApplication.class, args);
+	}
+
+}
---
+++ Pense-Bet/src/test/java/ch/arc/pensebet/PenseBetApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.arc.pensebet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PenseBetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

bad6173ad74a607a754e6063428172cb50155913
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-03-15 11:02:30 +0100

    Create Bet model

--- Pense-Bet/src/main/java/ch/arc/pensebet/model/Bet.java
+++ Pense-Bet/src/main/java/ch/arc/pensebet/model/Bet.java
@@ -1,5 +1,60 @@
 package ch.arc.pensebet.model;
 
-public class Bet {
+import java.util.Date;
 
+public class Bet {
+	public String subject;
+	public Date creationDate;
+	public Date endingDate;
+	public float amout;
+	public Boolean result = null;
+	public State state;
+	
+	public Bet() { }
+	
+	public Bet(String subject, Date endingDate, float amout, State state) {
+		this.subject = subject;
+		this.endingDate = endingDate;
+		this.amout = amout;
+		this.state = state;
+		
+		creationDate = new Date();
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public Date getEndingDate() {
+		return endingDate;
+	}
+	public void setEndingDate(Date endingDate) {
+		this.endingDate = endingDate;
+	}
+	public float getAmout() {
+		return amout;
+	}
+	public void setAmout(float amout) {
+		this.amout = amout;
+	}
+	public boolean isResult() {
+		return result;
+	}
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
 }

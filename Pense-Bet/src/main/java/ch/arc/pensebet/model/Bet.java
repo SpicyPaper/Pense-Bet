8c96acc208fb7194df5d091bdad551af9edda090
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-03-15 11:23:37 +0100

    Improve pom.xml, improve Bet model

--- Pense-Bet/src/main/java/ch/arc/pensebet/model/Bet.java
+++ Pense-Bet/src/main/java/ch/arc/pensebet/model/Bet.java
@@ -2,12 +2,40 @@ package ch.arc.pensebet.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="bet")
+
 public class Bet {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column
+	public Integer id;
+	
+	@Column
 	public String subject;
+	
+	@Column
 	public Date creationDate;
+	
+	@Column
 	public Date endingDate;
 	public float amout;
+	
+	@Column
 	public Boolean result = null;
+
+    @ManyToOne
+    @JoinColumn
 	public State state;
 	
 	public Bet() { }

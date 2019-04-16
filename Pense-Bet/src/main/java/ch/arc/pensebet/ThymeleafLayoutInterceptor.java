e23c091801da4560fbea9f98e96abec9ded1ae94
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-04-16 15:56:21 +0200

    Improve layout system

--- Pense-Bet/src/main/java/ch/arc/pensebet/ThymeleafLayoutInterceptor.java
+++ Pense-Bet/src/main/java/ch/arc/pensebet/ThymeleafLayoutInterceptor.java
@@ -3,12 +3,13 @@ package ch.arc.pensebet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.util.Assert;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class ThymeleafLayoutInterceptor extends HandlerInterceptorAdapter {
-	
-	private static final String DEFAULT_LAYOUT = "layouts/default";
+
+    private static final String DEFAULT_LAYOUT = "layouts/default";
     private static final String DEFAULT_VIEW_ATTRIBUTE_NAME = "template";
 
     @Override
@@ -16,9 +17,16 @@ public class ThymeleafLayoutInterceptor extends HandlerInterceptorAdapter {
         if (modelAndView == null || !modelAndView.hasView()) {
             return;
         }
-        
         String originalViewName = modelAndView.getViewName();
-        modelAndView.setViewName(DEFAULT_LAYOUT);
+        if (isRedirectOrForward(originalViewName)) {
+            return;
+        }
+        String layoutName = DEFAULT_LAYOUT;
+        modelAndView.setViewName(layoutName);
         modelAndView.addObject(DEFAULT_VIEW_ATTRIBUTE_NAME, originalViewName);
-    }  
+    }
+
+    private boolean isRedirectOrForward(String viewName) {
+        return viewName.startsWith("redirect:") || viewName.startsWith("forward:");
+    }
 }

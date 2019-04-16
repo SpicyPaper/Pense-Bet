24cb3d595405af9ffd0a9f260ad9932a0a679c31
Guillaume Chacun <guillaume.chacun@he-arc.ch>
2019-04-16 11:22:48 +0200

    Code cleaning in controllers

--- Pense-Bet/src/main/java/ch/arc/pensebet/controller/HomeController.java
+++ Pense-Bet/src/main/java/ch/arc/pensebet/controller/HomeController.java
@@ -2,12 +2,11 @@ package ch.arc.pensebet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import ch.arc.pensebet.model.User;
 import ch.arc.pensebet.service.IBetService;
 import ch.arc.pensebet.service.IUserService;
 
@@ -26,26 +25,30 @@ public class HomeController {
     }
 
     @GetMapping("/bet/user/waiting")
-    public String allBetsWaiting(@AuthenticationPrincipal User activeUser, Model model) {
-    	model.addAttribute("bets", betService.findPersonnalActive(activeUser, new PageRequest(0, 3)));
+    public String allBetsWaiting(Authentication authentication, Model model) {
+    	model.addAttribute("bets", betService.findPersonnalActive(userService.findUserByNickname(authentication.getName()), 
+    															  new PageRequest(0, 3)));
         return "index";
     }
 
     @GetMapping("/bet/user/active")
-    public String allBetsActive(@AuthenticationPrincipal User activeUser, Model model) {
-    	model.addAttribute("bets", betService.findPersonnalActive(activeUser, new PageRequest(0, 3)));
+    public String allBetsActive(Authentication authentication, Model model) {
+    	model.addAttribute("bets", betService.findPersonnalActive(userService.findUserByNickname(authentication.getName()), 
+				  												  new PageRequest(0, 3)));
         return "index";
     }
 
     @GetMapping("/bet/user/ended")
-    public String allBetsEnded(@AuthenticationPrincipal User activeUser, Model model) {
-    	model.addAttribute("bets", betService.findPersonnalActive(activeUser, new PageRequest(0, 3)));
+    public String allBetsEnded(Authentication authentication, Model model) {
+    	model.addAttribute("bets", betService.findPersonnalActive(userService.findUserByNickname(authentication.getName()), 
+				  												  new PageRequest(0, 3)));
         return "index";
     }
 
     @GetMapping("/bet/user/closed")
-    public String allBetsClosed(@AuthenticationPrincipal User activeUser, Model model) {
-    	model.addAttribute("bets", betService.findPersonnalActive(activeUser, new PageRequest(0, 3)));
+    public String allBetsClosed(Authentication authentication, Model model) {
+    	model.addAttribute("bets", betService.findPersonnalActive(userService.findUserByNickname(authentication.getName()), 
+				  												  new PageRequest(0, 3)));
         return "index";
     }
 }
--- Pense-Bet/src/main/java/ch/arc/pensebet/controller/PersonnalBetsController.java
+++ Pense-Bet/src/main/java/ch/arc/pensebet/controller/PersonnalBetsController.java
@@ -1,17 +1,12 @@
 package ch.arc.pensebet.controller;
 
-import java.security.Principal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import ch.arc.pensebet.model.User;
 import ch.arc.pensebet.service.IBetService;
 import ch.arc.pensebet.service.IUserService;
 
@@ -25,20 +20,23 @@ public class PersonnalBetsController {
 	IBetService betService;
 
     @GetMapping("/bet/user/created/active")
-    public String personnalBetsActive(@AuthenticationPrincipal User activeUser, Model model) {
-    	model.addAttribute("bets", betService.findPersonnalActive(activeUser, new PageRequest(0, 3)));
+    public String personnalBetsActive(Authentication authentication, Model model) {
+    	model.addAttribute("bets", betService.findPersonnalActive(userService.findUserByNickname(authentication.getName()), 
+				  												  new PageRequest(0, 3)));
         return "personnal-bets";
     }
 
     @GetMapping("/bet/user/created/ended")
-    public String personnalBetsEnded(@AuthenticationPrincipal User activeUser, Model model) {
-    	model.addAttribute("bets", betService.findPersonnalEnded(activeUser, new PageRequest(0, 3)));
+    public String personnalBetsEnded(Authentication authentication, Model model) {
+    	model.addAttribute("bets", betService.findPersonnalEnded(userService.findUserByNickname(authentication.getName()), 
+				  												 new PageRequest(0, 3)));
         return "personnal-bets";
     }
 
     @GetMapping("/bet/user/created/closed")
-    public String personnalBetsClosed(@AuthenticationPrincipal User activeUser, Model model) {
-    	model.addAttribute("bets", betService.findPersonnalClosed(activeUser, new PageRequest(0, 3)));
+    public String personnalBetsClosed(Authentication authentication, Model model) {
+    	model.addAttribute("bets", betService.findPersonnalClosed(userService.findUserByNickname(authentication.getName()), 
+				  												  new PageRequest(0, 3)));
         return "personnal-bets";
     }
 }

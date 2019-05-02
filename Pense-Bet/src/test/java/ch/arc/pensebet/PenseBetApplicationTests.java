fbe12c3f0d71b875eed04c48bd2d3f3cad42d470
Guillaume Chacun <guillaume.chacun@he-arc.ch>
2019-05-02 17:52:13 +0200

    Tests bet model

--- Pense-Bet/src/test/java/ch/arc/pensebet/PenseBetApplicationTests.java
+++ Pense-Bet/src/test/java/ch/arc/pensebet/PenseBetApplicationTests.java
@@ -2,11 +2,14 @@ package ch.arc.pensebet;
 
 import static org.junit.Assert.assertEquals;
 
+import java.sql.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import ch.arc.pensebet.model.Bet;
 import ch.arc.pensebet.model.User;
 
 @RunWith(SpringRunner.class)
@@ -31,7 +34,34 @@ public class PenseBetApplicationTests {
 		
 		assertEquals(email, user.getEmail());
 		assertEquals(nickname, user.getNickname());
-		assertEquals(money, user.getMoney());
+		assertEquals(money, user.getMoney(), 0.001);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testBetModel() {
+		float amount = 3.54f;
+		Date creationDate = new Date(2019, 5, 2);
+		Date endingDate = new Date(2019, 5, 4);
+		float moneyPerWinner = 2;
+		boolean result = true;
+		
+		Bet bet = new Bet();
+		bet.setAmount(amount);
+		bet.setCreationDate(creationDate);
+		bet.setEndingDate(endingDate);
+		bet.setMoneyPerWinner(moneyPerWinner);
+		bet.setResult(result);
+		
+		assertEquals(amount, bet.getAmount(), 0.001);
+		assertEquals(creationDate.getDay(), bet.getCreationDate().getDay());
+		assertEquals(creationDate.getMonth(), bet.getCreationDate().getMonth());
+		assertEquals(creationDate.getYear(), bet.getCreationDate().getYear());
+		assertEquals(endingDate.getDay(), bet.getEndingDate().getDay());
+		assertEquals(endingDate.getMonth(), bet.getEndingDate().getMonth());
+		assertEquals(endingDate.getYear(), bet.getEndingDate().getYear());
+		assertEquals(moneyPerWinner, bet.getMoneyPerWinner(), 0.01);
+		assertEquals(result, bet.getResult());
 	}
 
 }

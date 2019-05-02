6ad956c263305db97c16d1e2c76bbe9e25d2b83b
Guillaume Chacun <guillaume.chacun@he-arc.ch>
2019-05-02 16:41:29 +0200

    Tests user model

--- Pense-Bet/src/test/java/ch/arc/pensebet/PenseBetApplicationTests.java
+++ Pense-Bet/src/test/java/ch/arc/pensebet/PenseBetApplicationTests.java
@@ -1,9 +1,13 @@
 package ch.arc.pensebet;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import ch.arc.pensebet.model.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,23 @@ public class PenseBetApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testUserModel() {
+		String email = "g@pensebet.ch";
+		String nickname = "g";
+		float money = 10;
+		
+		User user = new User();
+		user.setEmail(email);
+		user.setNickname(nickname);
+		user.setMoney(money);
+		
+		assertEquals(email, user.getEmail());
+		assertEquals(nickname, user.getNickname());
+		assertEquals(money, user.getMoney());
 	}
 
 }
